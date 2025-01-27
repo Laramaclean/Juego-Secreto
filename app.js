ef01ce82e115afcefc2d24550271283186360205
Laramaclean <macleanlaramagali4@gmail.com>
2025-01-27 01:18:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -36,10 +36,13 @@ function limpiarCaja() {
 
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
     if (listaNumerosSorteados.length == numeroMaximo) {
         asignarTextoElemento("p", "Ya se sortearon todos los números posibles");
-    } else{
+    } else {
         //Si el número geneeado está incluido en la lista
         if (listaNumerosSorteados.includes(numeroGenerado)) {
             return generarNumeroSecreto();
